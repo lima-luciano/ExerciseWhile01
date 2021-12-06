f807ea007c9d61c50d6a17dc52f73ff860717bd2
Devllu <dev.limaluciano@gmail.com>
2021-12-06 00:09:23 -0300

    Add project files.

---
+++ ExerciseWhile01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExerciseWhile01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Password");
+            int pass = int.Parse(Console.ReadLine());
+
+            while (pass != 2021)
+            {
+                Console.WriteLine("Invalid Password");
+                Console.WriteLine("Enter Password");
+                pass = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Allowed Access");
+
+        }
+    }
+}
+// Solved by lima-luciano on 11/16/2021
